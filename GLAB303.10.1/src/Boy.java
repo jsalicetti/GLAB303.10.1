099f0b0fe9ae2a0d446bb68abf299f22166f7e31
jsalicetti <jessicasalicetti@yahoo.com>
2024-07-17 09:55:48 -0400

    completed

---
+++ GLAB303.10.1/src/Boy.java
@@ -0,0 +1,10 @@
+public class Boy extends Person {
+
+    public String talk() {
+        return(super.talk() + " ... but I love Java class.");
+    }
+    public String walk() {
+        return("I am now walking");
+    }
+}
+
